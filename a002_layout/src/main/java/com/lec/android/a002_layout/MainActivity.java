e8c7f50d72b02a6b69e8e3f78de9f268ce5452a6
gksfk6165 <gksfk308@gmail.com>
2020-04-14 10:20:45 +0900

    첫 커밋 2020-04-14

---
+++ a002_layout/src/main/java/com/lec/android/a002_layout/MainActivity.java
@@ -0,0 +1,31 @@
+package com.lec.android.a002_layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    int count = 90;
+    Button btn;
+    ImageView img;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.turn);
+
+
+        btn = (Button) findViewById(R.id.btn1);
+        img = (ImageView) findViewById(R.id.imageView);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                img.setRotation(count);
+                count+=90;
+            }
+        });
+    }
+}
