25eab8c65ed9aa28e60d0e875fe50facd52ab346
gksfk6165 <gksfk308@gmail.com>
2020-04-15 18:19:13 +0900

    학원커밋3-1

--- a003_listener/src/main/java/com/lec/android/a003_listener/Main2Activity.java
+++ a003_listener/src/main/java/com/lec/android/a003_listener/Main2Activity.java
@@ -1,11 +1,10 @@
-package com.lec.android.a003_widget;
+package com.lec.android.a003_listener;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
